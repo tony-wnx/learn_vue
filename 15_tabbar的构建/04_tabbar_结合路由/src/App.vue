bd342c5cdb8418e180bdbb7b24a4c55d3734091f
yxq <yangxiongqian@jszyedu.cn>
2020-02-28 13:32:14 +0800

    file path aliase update

--- 15_tabbar的构建/04_tabbar_结合路由/src/App.vue
+++ 15_tabbar的构建/04_tabbar_结合路由/src/App.vue
@@ -5,8 +5,8 @@
       <!-- 4、父组件传递路由地址到子组件中,添加path(因为这里是固定值,所以不用:绑定,如果是使用组件中data中的值就需要动态绑定了) -->
       <!-- 5-1、自定义字体颜色 <Tabba-item link="/home"> -->
       <Tabba-item link="/home" activeColor="red">
-        <img slot="item-icon" src="./assets/img/tabbar/home.svg">
-        <img slot="item-icon-active" src="./assets/img/tabbar/home-active.svg">
+        <img slot="item-icon" src="~img/tabbar/home.svg">
+        <img slot="item-icon-active" src="~img/tabbar/home-active.svg">
         <div slot="item-text">首页</div>
       </Tabba-item>
       <Tabba-item link="/category">
@@ -29,8 +29,8 @@
 </template>
 
 <script>
-  import Tabbar from './components/tabbar/Tabbar'
-  import TabbaItem from './components/tabbar/TabbarItem'
+  import Tabbar from 'components/tabbar/Tabbar'
+  import TabbaItem from 'components/tabbar/TabbarItem'
 
   export default {
     name: 'App',
