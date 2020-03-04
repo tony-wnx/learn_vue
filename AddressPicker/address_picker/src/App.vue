702dbb0d83c9fbe7134001a7ee61fb0eb21827b2
yxq <yangxiongqian@jszyedu.cn>
2020-03-04 22:20:31 +0800

    addressPicker add

---
+++ AddressPicker/address_picker/src/App.vue
@@ -0,0 +1,24 @@
+<!--  -->
+<template>
+  <div id="app">
+    <AddressPicker></AddressPicker>
+  </div>
+</template>
+
+<script>
+  import AddressPicker from './components/AddressPicker'
+  export default {
+    components: {
+      AddressPicker
+    },
+    data() {
+      return {};
+    },
+    computed: {},
+    watch: {},
+    methods: {}
+  }
+</script>
+
+<style>
+</style>
